39b2a499fba26dcb572e42a74f080356cf2d94fb
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-26 23:58:24 +0600

    mobile nav create

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,25 +5,28 @@ function Header() {
 	return (
 		<header className="bg-violet-800">
 			<nav className="container mx-auto">
-				<div className="flex justify-between items-center h-24">
+				<div className="flex justify-between items-center h-[13vh] px-4 md:px-0">
 					<Link href="/">
-						<a className="flex">
+						<a className="flex w-[5.2rem] md:w-auto">
 							<Image
 								src="/images/logo.jpg"
-								width={128}
-								height={72}
+								width={120}
+								height={60}
 								alt="Logo"
 								className="rounded-full"
 							/>
 						</a>
 					</Link>
-					<ul className="flex justify-evenly w-[70%]">
-						<li className="nav__list group">
+					<svg className="w-6 h-6 md:hidden fill-white">
+						<use xlinkHref="/images/sprite.svg#menu"></use>
+					</svg>
+					<ul className="md:justify-evenly md:w-[70%] w-full flex md:flex-row flex-col md:gap-0 gap-5 pt-5 md:pt-0 bg-black md:bg-inherit fixed md:top-auto top-[13vh] right-0 h-[87vh] md:h-auto translate-x-0 md:translate-x-0 transition-transform duration-500 px-4 md:px-0">
+						<li className="nav__list group lnk">
 							<Link href="/games">
 								<a className="nav__item">Games</a>
 							</Link>
 							<div className="nav__sub-menu">
-								<ul>
+								<ul className="md:bg-black bg-white text-black md:text-white font-semibold relative z-10 p-5 rounded-lg">
 									<li>
 										<Link href="/games/game-id">
 											<a className="hover:text-cyan-400">
@@ -48,14 +51,14 @@ function Header() {
 								</ul>
 							</div>
 						</li>
-						<li className="nav__list group">
+						<li className="nav__list group lnk">
 							<Link href="/gift-cards">
 								<a className="nav__item">
 									Gift Cards
 								</a>
 							</Link>
 							<div className="nav__sub-menu">
-								<ul>
+								<ul className="md:bg-black bg-white text-black md:text-white font-semibold relative z-10 p-5 rounded-lg">
 									<li>
 										<Link href="/gift-cards/gift-cards-id">
 											<a className="hover:text-cyan-400">
@@ -80,14 +83,14 @@ function Header() {
 								</ul>
 							</div>
 						</li>
-						<li className="nav__list group">
+						<li className="nav__list lnk">
 							<Link href="/games">
 								<a className="nav__item">
 									Razor Gold
 								</a>
 							</Link>
 						</li>
-						<li className="nav__list group">
+						<li className="nav__list lnk">
 							<Link href="/games">
 								<a className="nav__item">Offers</a>
 							</Link>
