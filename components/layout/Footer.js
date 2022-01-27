f85690a2e8dc1cf33b79fc5398e40ffdea0240d9
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-28 00:47:06 +0600

    Need to Add Social Icons

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,5 +1,47 @@
 function Footer() {
-	return <div className="text-2xl">Enter</div>;
+	return (
+		<footer className="bg-black py-12">
+			<div className="container mx-auto">
+				<div className="flow-root">
+					<div className="flex-1">
+						<ul className="flex text-white text-xl justify-evenly">
+							<li>
+								<a className="lnk" href="#null">
+									About Us
+								</a>
+							</li>
+							<li>
+								<a className="lnk" href="#null">
+									Blog
+								</a>
+							</li>
+							<li>
+								<a className="lnk" href="#null">
+									Press
+								</a>
+							</li>
+							<li>
+								<a className="lnk" href="#null">
+									Contact
+								</a>
+							</li>
+							<li>
+								<a className="lnk" href="#null">
+									App
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className="flex-1"></div>
+				</div>
+				<div className="text-center mt-10">
+					<p className="text-[#9b59b6] font-semibold text-lg">
+						&copy; Nayeem
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 function index() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
 }
 
 export default index;
