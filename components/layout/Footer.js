96696be39c180e25f0c5c0167ea089b668576cc1
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-30 00:01:16 +0600

    Footer complete

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -2,9 +2,9 @@ function Footer() {
 	return (
 		<footer className="bg-black py-12">
 			<div className="container mx-auto">
-				<div className="flow-root">
-					<div className="flex-1">
-						<ul className="flex text-white text-xl justify-evenly">
+				<div className="flex">
+					<div className="w-[45%]">
+						<ul className="flex text-white text-xl justify-between">
 							<li>
 								<a className="lnk" href="#null">
 									About Us
@@ -32,7 +32,23 @@ function Footer() {
 							</li>
 						</ul>
 					</div>
-					<div className="flex-1"></div>
+					<div className="w-[55%] flex justify-end gap-3">
+						<a href="#null">
+							<svg className="w-8 h-8 fill-transparent stroke-white hover:stroke-cyan-500 transition">
+								<use xlinkHref="/images/sprite.svg#twitter"></use>
+							</svg>
+						</a>
+						<a href="#null">
+							<svg className="w-8 h-8 fill-transparent stroke-white hover:stroke-cyan-500 transition">
+								<use xlinkHref="/images/sprite.svg#facebook"></use>
+							</svg>
+						</a>
+						<a href="#null">
+							<svg className="w-8 h-8 stroke-white fill-white hover:stroke-cyan-500 transition">
+								<use xlinkHref="/images/sprite.svg#instagram"></use>
+							</svg>
+						</a>
+					</div>
 				</div>
 				<div className="text-center mt-10">
 					<p className="text-[#9b59b6] font-semibold text-lg">
