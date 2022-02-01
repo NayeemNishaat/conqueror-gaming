73956e474b0b8dc1699f5347e8d2504974fa6b5a
nayeemjc <RxNayeemIMI@gmail.com>
2022-02-01 20:42:18 +0600

    Adding Data to the Dynamic Paths

--- components/product-details/ProductDetails.js
+++ components/product-details/ProductDetails.js
@@ -2,4 +2,13 @@ function ProductDetails() {
 	return <div>Enter</div>;
 }
 
+export const getStaticProps = async (ctx) => {
+	console.log(45);
+	return {
+		props: {
+			data: null
+		}
+	};
+};
+
 export default ProductDetails;
