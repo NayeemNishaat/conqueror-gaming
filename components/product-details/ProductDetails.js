770c13f117fa67b847c9b5902e0e6bbbd4a95e0f
nayeemjc <RxNayeemIMI@gmail.com>
2022-03-10 02:17:01 +0600

    updated checkout form

--- components/product-details/ProductDetails.js
+++ components/product-details/ProductDetails.js
@@ -36,7 +36,7 @@ function ProductDetails(props) {
 		const email = emailRef.current.value;
 		const name = nameRef.current.value;
 
-		dispatch("setOrder", { uid, email });
+		dispatch("setOrder", { uid, email, userName: name });
 
 		router.push({
 			pathname: "/checkout",
@@ -66,125 +66,123 @@ function ProductDetails(props) {
 				</div>
 
 				<div className="flex-1 flex flex-col">
-					<div className="flex flex-col gap-5">
-						<div className="flex items-center gap-5">
-							<label
-								htmlFor="uid"
-								className="text-lg font-semibold w-[55%]"
-							>
-								Enter UID
-							</label>
-							<input
-								className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
-								id="uid"
-								type="text"
-								ref={uidRef}
-								required
-							/>
+					<form onSubmit={btnClickHandler}>
+						<div className="flex flex-col gap-5">
+							<div className="flex items-center gap-5">
+								<label
+									htmlFor="uid"
+									className="text-lg font-semibold w-[55%]"
+								>
+									Enter UID
+								</label>
+								<input
+									className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
+									id="uid"
+									type="text"
+									ref={uidRef}
+									required
+								/>
+							</div>
+							<div className="flex items-center gap-5">
+								<label
+									htmlFor="uid"
+									className="text-lg font-semibold w-[55%]"
+								>
+									Enter Name
+								</label>
+								<input
+									className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
+									id="uid"
+									type="text"
+									ref={nameRef}
+									required
+								/>
+							</div>
+							<div className="flex items-center gap-5">
+								<label
+									htmlFor="email"
+									className="text-lg font-semibold  w-[55%]"
+								>
+									Enter Email
+								</label>
+								<input
+									className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
+									id="email"
+									type="email"
+									ref={emailRef}
+									required
+								/>
+							</div>
+
+							<div className="flex items-center gap-5">
+								<label className="text-lg font-semibold w-24">
+									Amount
+								</label>
+								<span className="text-3xl text-green-500 font-bold">
+									{props.product.amount}
+								</span>
+							</div>
+							<div className="flex items-center gap-5">
+								<label className="text-lg font-semibold w-24">
+									Price
+								</label>
+								<span className="text-3xl text-orange-600 font-bold">
+									{props.product.price}
+								</span>
+							</div>
 						</div>
-						<div className="flex items-center gap-5">
-							<label
-								htmlFor="uid"
-								className="text-lg font-semibold w-[55%]"
-							>
-								Enter UID
-							</label>
-							<input
-								className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
-								id="uid"
-								type="text"
-								ref={uidRef}
-								required
-							/>
+						<p className="text-lg font-semibold my-5">
+							Select Payment Method:
+						</p>
+						<div className="flex md:flex-row flex-col gap-5 justify-center items-center">
+							<figure>
+								<input
+									type="radio"
+									defaultChecked
+									name="pay-method"
+									id="bkash"
+									value="bkash"
+									className="peer hidden"
+									onChange={setPaymentHandler}
+								/>
+								<label
+									htmlFor="bkash"
+									className="flex flex-col items-center peer-checked:ring-2 cursor-pointer peer-checked:ring-rose-600 rounded-xl"
+								>
+									<svg className="w-40 h-20">
+										<use xlinkHref="/images/common/sprite.svg#bkash"></use>
+									</svg>
+									<figcaption className="text-lg font-semibold">
+										bKash
+									</figcaption>
+								</label>
+							</figure>
+							<figure>
+								<input
+									type="radio"
+									name="pay-method"
+									id="card"
+									value="card"
+									className="peer hidden"
+									onChange={setPaymentHandler}
+								/>
+								<label
+									htmlFor="card"
+									className="flex flex-col items-center peer-checked:ring-2 cursor-pointer peer-checked:ring-rose-600 rounded-xl"
+								>
+									<svg className="w-40 h-20 py-2.5">
+										<use xlinkHref="/images/common/sprite.svg#card"></use>
+									</svg>
+									<figcaption className="text-lg font-semibold">
+										Card
+									</figcaption>
+								</label>
+							</figure>
 						</div>
-						<div className="flex items-center gap-5">
-							<label
-								htmlFor="email"
-								className="text-lg font-semibold  w-[55%]"
-							>
-								Enter Email
-							</label>
-							<input
-								className="bg-gray-200 outline-none rounded-sm ring-1 ring-cyan-400 py-1 px-3 w-full"
-								id="email"
-								type="email"
-								ref={emailRef}
-								required
-							/>
-						</div>
-						<div className="flex items-center gap-5">
-							<label className="text-lg font-semibold w-24">
-								Amount
-							</label>
-							<span className="text-3xl text-green-500 font-bold">
-								{props.product.amount}
-							</span>
-						</div>
-						<div className="flex items-center gap-5">
-							<label className="text-lg font-semibold w-24">
-								Price
-							</label>
-							<span className="text-3xl text-orange-600 font-bold">
-								{props.product.price}
-							</span>
-						</div>
-					</div>
-					<p className="text-lg font-semibold my-5">
-						Select Payment Method:
-					</p>
-					<div className="flex md:flex-row flex-col gap-5 justify-center items-center">
-						<figure>
-							<input
-								type="radio"
-								defaultChecked
-								name="pay-method"
-								id="bkash"
-								value="bkash"
-								className="peer hidden"
-								onChange={setPaymentHandler}
-							/>
-							<label
-								htmlFor="bkash"
-								className="flex flex-col items-center peer-checked:ring-2 cursor-pointer peer-checked:ring-rose-600 rounded-xl"
-							>
-								<svg className="w-40 h-20">
-									<use xlinkHref="/images/common/sprite.svg#bkash"></use>
-								</svg>
-								<figcaption className="text-lg font-semibold">
-									bKash
-								</figcaption>
-							</label>
-						</figure>
-						<figure>
-							<input
-								type="radio"
-								name="pay-method"
-								id="card"
-								value="card"
-								className="peer hidden"
-								onChange={setPaymentHandler}
-							/>
-							<label
-								htmlFor="card"
-								className="flex flex-col items-center peer-checked:ring-2 cursor-pointer peer-checked:ring-rose-600 rounded-xl"
-							>
-								<svg className="w-40 h-20 py-2.5">
-									<use xlinkHref="/images/common/sprite.svg#card"></use>
-								</svg>
-								<figcaption className="text-lg font-semibold">
-									Card
-								</figcaption>
-							</label>
-						</figure>
-					</div>
-					<div className="text-center mt-10">
-						<a
-							className="bg-[#44d62c] block py-2 px-3 rounded-sm hover:bg-[#71f85c] transition text-xl font-semibold cursor-pointer"
-							onClick={btnClickHandler}
-						>
+						<button className="mt-10 w-full bg-[#44d62c] block py-2 px-3 rounded-sm hover:bg-[#71f85c] transition text-xl font-semibold cursor-pointer">
 							Checkout
-						</a>
-					</div>
+						</button>
+					</form>
 				</div>
 				{/* <div className="relative">
 					<div className="absolute top-0 left-0 -z-10 w-full h-full">
